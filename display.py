2e4becf1b5531473cbacabf5ec6245f9bf47e9a0
ddoberne <ddoberne@gmail.com>
2022-04-07 22:40:28 -0700

    Update display.py

--- display.py
+++ display.py
@@ -13,7 +13,6 @@ import requests
 
 # In[32]:
 
-import datetime
 from datetime import date, timedelta
 
 st.write('# The Filthiest ⚾')
@@ -25,14 +24,14 @@ st.write('# The Filthiest ⚾')
 
 # In[ ]:
 
-date = datetime.now() + timedelta(hours = -8)
+date = datetime.date() + timedelta(days = -1)
 try:
   
   date_str = '-'.join([str(date.year), str(date.month), str(date.day)])
   filename = date_str + '.csv'
   url = 'https://storage.googleapis.com/the-filthiest/pitch-data/' + filename
 except:
-  date = datetime.now() + timedelta(days = -1) + timedelta(hours = -8)
+  date = datetime.date() + timedelta(days = -2)
   date_str = '-'.join([str(date.year), str(date.month), str(date.day)])
   filename = date_str + '.csv'
   url = 'https://storage.googleapis.com/the-filthiest/pitch-data/' + filename
