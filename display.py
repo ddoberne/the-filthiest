1c9334a4879eddb56b5e069de4b9ad75458ed996
ddoberne <ddoberne@gmail.com>
2022-05-03 11:16:02 -0700

    Update display.py

--- display.py
+++ display.py
@@ -128,7 +128,7 @@ if len(leaderboard) > 0:
     if pitch_type == '(none)':
       st.write(f'The top {str(show_n)} pitches from MLB games on {pick_date}, sorted by {sort_in}.')
     else:
-      st.write(f'The top {str(show_n)} {pitch_type_in}s from MLB games on {date}, sorted by {sort_in}.')
+      st.write(f'The top {str(show_n)} {pitch_type_in}s from MLB games on {pick_date}, sorted by {sort_in}.')
     st.dataframe(leaderboard_show.head(show_n).style.format({'Velo (mph)':"{:.4}", 'FiFaX':"{:.3}"}))
     
     if leader_index <= show_n:
